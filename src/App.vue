f734f3f16e42ba6e5b098ed458815e55463386d9
gitmenew <gitnew22@gmx.us>
2025-06-09 19:03:23 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -113,10 +113,18 @@ function cancelHold() {
   display: flex;
   justify-content: center;
   align-items: center;
-  min-height: 94vh;
-  padding: 1rem;
+  min-height: 90vh;
+  padding: 2rem;
   background-color: #f8f8f8;
   font-family: Arial, sans-serif;
+}
+.adobe-sign-container {
+  max-width: 350px;
+  margin: auto;
+  background: white;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0,0,0,0.1);
+  padding: 2rem;
 }
 
 
