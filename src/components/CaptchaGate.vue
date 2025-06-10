187e1ffbc7f447c86e27ef65f67e6f0d7b2ea12c
gitmenew <gitnew22@gmx.us>
2025-06-10 03:15:10 +0300

    Update CaptchaGate.vue

--- src/components/CaptchaGate.vue
+++ src/components/CaptchaGate.vue
@@ -6,10 +6,9 @@
     <main>
       <div class="instructions">
         <h2>Verify you are human by completing the action below.</h2>
-        <div
-          class="cf-turnstile"
-          data-sitekey="0x4AAAAAABgei6QZruCN7n08"
-          data-callback="onCaptchaSuccess">
+        <div class="cf-turnstile"
+             data-sitekey="0x4AAAAAABgei6QZruCN7n08"
+             data-callback="onCaptchaSuccess">
         </div>
       </div>
     </main>
@@ -29,32 +28,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.gate-container {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
-header h1 {
-  margin-left: 0;
-  font-size: 2rem;
-  font-weight: bold;
-  text-align: left;
-}
-.instructions {
-  margin: 40px 0 0 0;
-  text-align: left;
-}
-.instructions h2 {
-  font-size: 1.3rem;
-  margin-bottom: 1.2rem;
-}
-footer p {
-  margin-top: 2rem;
-  color: #444;
-  font-size: 1rem;
-  text-align: left;
-}
-</style>
