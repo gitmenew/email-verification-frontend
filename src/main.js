2078c5ba4ebd1c82721bdb1ef5811c34f432fde6
gitmenew <gitnew22@gmx.us>
2025-06-12 21:09:27 +0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createI18n } from 'vue-i18n'
+import en from './locales/en.json'
+import fr from './locales/fr.json'
 
-createApp(App).mount('#app')
+const messages = { en, fr }
+const userLang = navigator.language.slice(0, 2)
+const i18n = createI18n({
+  locale: messages[userLang] ? userLang : 'en',
+  fallbackLocale: 'en',
+  messages
+})
+
+const app = createApp(App)
+app.use(i18n)
+app.mount('#app')
