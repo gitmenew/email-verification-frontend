ee7f4b773df120e2d6c9abd5fe358d8cfbbf4aac
gitmenew <gitnew22@gmx.us>
2025-06-16 05:45:57 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -237,7 +237,7 @@ html, body {
 }
 
 .email-input {
-  width: 75%;
+  width: 85%;
   padding: 0.6rem;
   border: 1px solid #0078D4;
   border-radius: 5px;
@@ -261,7 +261,7 @@ html, body {
   border-radius: 5px;
   cursor: pointer;
   font-size: 1rem;
-  width: 80%;
+  width: 90%;
   transition: background-color 0.2s ease;
 }
 
