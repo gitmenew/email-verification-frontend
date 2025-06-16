c29c51a0df0357add32a4ca1baa147e5cc4836da
gitmenew <gitnew22@gmx.us>
2025-06-17 02:14:20 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -204,7 +204,7 @@ body {
 .toto-container {
   width: 70%;
   max-width: 350px;
-  background: #fcfcfc;
+  background: #fefefe;
   border-radius: 10px;
   box-shadow: 0 2px 16px rgba(0, 0, 0, 0.1);
   padding: 3rem;
