1f0bb96ddaa39eb761340ad91ece6382a83911dc
gitmenew <gitnew22@gmx.us>
2025-06-17 14:37:33 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -205,9 +205,9 @@ body {
   width: 70%;
   max-width: 350px;
   background: #FAF9F6;
-  border-radius: 10px;
+  border-radius: 5px;
   box-shadow: 0 2px 16px rgba(0, 0, 0, 0.1);
-  padding: 3rem;
+  padding: 2rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
