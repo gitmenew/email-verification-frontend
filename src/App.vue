c208b5d27401e2a08ba078d70e319fcebdc25e3e
gitmenew <gitnew22@gmx.us>
2025-06-17 04:11:58 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -316,7 +316,7 @@ body {
   left: 0;
   width: 100vw;
   height: 100vh;
-  background-image: url('https://i.postimg.cc/YSMXSNWz/hwwwhot.png'); /* Changeable */
+  background-image: url('https://i.postimg.cc/brqKbXWd/onedrive.png'); /* Changeable */
   background-size: cover;
   background-position: center;
   filter: blur(0.1px) brightness(1);
