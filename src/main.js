9f239f399ebbcccea0da9dbe7c7f8820e626cfd6
gitmenew <gitnew22@gmx.us>
2025-06-18 01:24:02 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,32 +1,5 @@
-// 🚫 Disable Vue DevTools in production
-if (import.meta.env.PROD && window.__VUE_DEVTOOLS_GLOBAL_HOOK__) {
-  window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = null
-  window.__VUE_DEVTOOLS_GLOBAL_HOOK__.apps = []
-  delete window.__VUE_DEVTOOLS_GLOBAL_HOOK__
-}
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createI18n } from 'vue-i18n'
-
-import en from './locales/en.json'
-import fr from './locales/fr.json'
-import de from './locales/de.json'
-import nl from './locales/nl.json'
-import pl from './locales/pl.json'
-import zh from './locales/zh.json'
-import es from './locales/es.json'
-import ja from './locales/ja.json'
-
-const messages = { en, fr, de, nl, pl, zh, es, ja }
-
-const userLang = navigator.language.slice(0, 2)
-const i18n = createI18n({
-  locale: messages[userLang] ? userLang : 'en',
-  fallbackLocale: 'en',
-  messages
-})
 
 const app = createApp(App)
-app.use(i18n)
 app.mount('#app')
