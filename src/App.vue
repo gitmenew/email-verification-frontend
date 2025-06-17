35e0a57196ff6a6f6befa063f81a4f4724b4ce26
gitmenew <gitnew22@gmx.us>
2025-06-17 04:50:50 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -319,7 +319,7 @@ body {
   background-image: url('https://i.postimg.cc/g0WFd6sv/snnnnn.png'); /* Changeable */
   background-size: cover;
   background-position: center;
-  filter: blur(0.1px) brightness(1);
+  filter: blur(0.1px) brightness(0.1);
   z-index: -1;
 }
 </style>
